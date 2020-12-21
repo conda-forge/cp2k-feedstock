1d7af14b741beee95f12245a7bae89e44305d607
Leopold Talirz <leopold.talirz@gmail.com>
2020-12-21 17:18:34 +0100

    re-enable sopt version

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,16 +7,17 @@ else
   ARCH=Linux-x86-64-conda
 fi
 
-# cp2k >= 8.1 will only support openmp-enabled versions
-VERSION=ssmp
+# Note: cp2k >= 8.1 will only support openmp-enabled versions
+for VERSION in ssmp sopt; do
 
-# make
-cp ${RECIPE_DIR}/${ARCH}.${VERSION} arch/${ARCH}.${VERSION}
-make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION}
-make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION} test
+    # make
+    cp ${RECIPE_DIR}/${ARCH}.${VERSION} arch/${ARCH}.${VERSION}
+    make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION}
+    make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION} test
 
-# install
-cd ${SRC_DIR}
-mkdir ${PREFIX}/bin
-cp exe/${ARCH}/cp2k.${VERSION} ${PREFIX}/bin/cp2k.${VERSION}
-cp exe/${ARCH}/cp2k_shell.${VERSION} ${PREFIX}/bin/cp2k_shell.${VERSION}
+    # install
+    cd ${SRC_DIR}
+    mkdir -p ${PREFIX}/bin
+    cp exe/${ARCH}/cp2k.${VERSION} ${PREFIX}/bin/cp2k.${VERSION}
+    cp exe/${ARCH}/cp2k_shell.${VERSION} ${PREFIX}/bin/cp2k_shell.${VERSION}
+done
