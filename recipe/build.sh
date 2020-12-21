1c057b51264ea8433ccc1554cfca2a5f4533e5c9
Leopold Talirz <leopold.talirz@gmail.com>
2020-12-21 21:28:34 +0100

    remove sopt build again
    the system environment variables for the compiler already contain
    -fopenmp, i.e. we would have to hack around this.
    
    let's just make a symlink instead

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,17 +7,19 @@ else
   ARCH=Linux-x86-64-conda
 fi
 
+VERSION=ssmp
+
+# make
+cp ${RECIPE_DIR}/${ARCH}.${VERSION} arch/${ARCH}.${VERSION}
+make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION}
+make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION} test
+
+# install
+cd ${SRC_DIR}
+mkdir -p ${PREFIX}/bin
+cp exe/${ARCH}/cp2k.${VERSION} ${PREFIX}/bin/cp2k.${VERSION}
+cp exe/${ARCH}/cp2k_shell.${VERSION} ${PREFIX}/bin/cp2k_shell.${VERSION}
+
+# Symlink cp2k.sopt for backwards compatibility of conda-forge package
 # Note: cp2k >= 8.1 will only support openmp-enabled versions
-for VERSION in ssmp sopt; do
-
-    # make
-    cp ${RECIPE_DIR}/${ARCH}.${VERSION} arch/${ARCH}.${VERSION}
-    make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION}
-    make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION} test
-
-    # install
-    cd ${SRC_DIR}
-    mkdir -p ${PREFIX}/bin
-    cp exe/${ARCH}/cp2k.${VERSION} ${PREFIX}/bin/cp2k.${VERSION}
-    cp exe/${ARCH}/cp2k_shell.${VERSION} ${PREFIX}/bin/cp2k_shell.${VERSION}
-done
+ln -s ${PREFIX}/bin/cp2k.${VERSION} ${PREFIX}/bin/cp2k.sopt
