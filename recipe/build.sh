5f7d1eb01963adedb2809f38c5d9fa19a524f646
Leopold Talirz <leopold.talirz@gmail.com>
2021-01-11 15:52:43 +0100

    fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,7 +17,7 @@ make -j${CPU_COUNT} ARCH=${ARCH} VERSION=${VERSION}
 # run regression tests
 # -mca plm isolated is needed to stop openmpi from looking for ssh
 # See https://github.com/open-mpi/ompi/issues/1838#issuecomment-229914599
-make ARCH=${ARCH} VERSION=${VERSION} TESTOPTS="-ompthreads 1 -mpiexec 'mpiexec --bind-to-none -mca plm isolated'" test
+make ARCH=${ARCH} VERSION=${VERSION} TESTOPTS="-ompthreads 1 -mpiexec 'mpiexec --bind-to none -mca plm isolated'" test
 
 # install
 cd ${SRC_DIR}
