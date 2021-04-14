65e0efdf9527bd665c1dd889702be0c3d1086e32
Leopold Talirz <leopold.talirz@gmail.com>
2021-04-14 22:25:25 +0200

    recreate directory structure for regtests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,11 +3,16 @@ set -e
 
 source ./define_vars.sh
 
+# create directory structure for regtest
+cp ${RECIPE_DIR}/${ARCH}.${VERSION} arch/${ARCH}.${VERSION}
+mkdir -p exe/${ARCH}
+ln -s ${PREFIX}/bin/cp2k.${VERSION} exe/${ARCH}
+
 # run regression tests
 if [[ "$mpi" == "nompi" ]]; then
-  make ARCH=${ARCH} VERSION=${VERSION} TESTOPTS="-ompthreads 2" test
+  make ARCH=${ARCH} VERSION=${VERSION} TESTOPTS="-nobuild -ompthreads 2" test
 else
   # -mca plm isolated is needed to stop openmpi from looking for ssh
   # See https://github.com/open-mpi/ompi/issues/1838#issuecomment-229914599
-  make ARCH=${ARCH} VERSION=${VERSION} TESTOPTS="-ompthreads 1 -mpiexec 'mpiexec --bind-to none -mca plm isolated'" test
+  make ARCH=${ARCH} VERSION=${VERSION} TESTOPTS="-nobuild -ompthreads 1 -mpiexec 'mpiexec --bind-to none -mca plm isolated'" test
 fi
