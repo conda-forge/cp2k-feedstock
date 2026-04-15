2129fd2fff6e5402939296691974acdcd707d1ae
Matthias Krack <matthias.krack@psi.ch>
2026-04-15 09:34:39 +0200

    Apply shellcheck to build.sh and fix ln command

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Build CP2K
-export PKG_CONFIG_PATH=${PREFIX}/lib:${PKG_CONFIG_PATH}
+export PKG_CONFIG_PATH="${PREFIX}/lib:${PKG_CONFIG_PATH}"
 cmake -B build -S . \
   -GNinja \
   -DCMAKE_BUILD_TYPE="Release" \
@@ -18,15 +18,15 @@ cmake -B build -S . \
   -DCP2K_USE_MPI="ON" \
   -DCP2K_USE_MPI_F08="ON" \
   -DCP2K_USE_SPGLIB="ON"
-cmake --build build --parallel ${CPU_COUNT}
+cmake --build build --parallel "${CPU_COUNT}"
 cmake --install build
-ln -s ${PREFIX}/bin/cp2k.psmp ${PREFIX}/bin/cp2k
+ln -sf cp2k.psmp "${PREFIX}/bin/cp2k"
 
 # Run CP2K regression tests
 ulimit -c 0 -s unlimited
-export CP2K_DATA_DIR=${PREFIX}/share/cp2k/data
+export CP2K_DATA_DIR="${PREFIX}/share/cp2k/data"
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 export OMPI_MCA_plm_rsh_agent=/bin/false
 export OMP_STACKSIZE=256M
-${PWD}/tests/do_regtest.py ${PREFIX}/bin psmp --maxtasks ${CPU_COUNT} --skipdir QS/regtest-dcdft-hfx --workbasedir ${BUILD_PREFIX}
+"${PWD}/tests/do_regtest.py" "${PREFIX}/bin" "psmp" --maxtasks "${CPU_COUNT}" --skipdir "QS/regtest-dcdft-hfx" --workbasedir "${BUILD_PREFIX}"
