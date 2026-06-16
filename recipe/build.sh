68dc27bc854b64979ef629b96064bf0c72d79c6d
Matthias Krack <matthias.krack@psi.ch>
2026-06-16 14:24:21 +0200

    Drop cp2k -v test
    - A crash in libgfortran occurs within the tests environment when the "cp2k -v" is run
      to print version and compilation information. The crash is most likely caused by the
      compiler_options() function from iso_fortran_env.
    - Replace some standard CMake flags by ${CMAKE_ARGS}
    - Tighten constraint for libtorch to "cpu_generic"
    - Bump build number to 12
    - MNT: Re-rendered with conda-smithy 2026.6.14 and conda-forge-pinning 2026.06.16.09.36.50

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,11 +13,7 @@ fi
 # Build CP2K
 export PKG_CONFIG_PATH="${PREFIX}/lib:${PKG_CONFIG_PATH}"
 cmake -B build -S . \
-  -GNinja \
-  -DCMAKE_BUILD_TYPE="Release" \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_SKIP_RPATH="ON" \
-  -DCMAKE_VERBOSE_MAKEFILE="OFF" \
+  ${CMAKE_ARGS} \
   -DCP2K_BLAS_VENDOR="OpenBLAS" \
   -DCP2K_USE_EVERYTHING="OFF" \
   -DCP2K_USE_ELPA="${CP2K_USE_ELPA}" \
@@ -34,7 +30,8 @@ cmake -B build -S . \
   -DCP2K_USE_SPGLIB="ON" \
   -DCP2K_USE_SPLA="ON" \
   -DCP2K_USE_TBLITE="OFF" \
-  -DCP2K_USE_TREXIO="ON"
+  -DCP2K_USE_TREXIO="ON" \
+  -GNinja
 cmake --build build --parallel "${CPU_COUNT}"
 cmake --install build
 ln -sf cp2k.psmp "${PREFIX}/bin/cp2k.popt"
@@ -46,11 +43,8 @@ export UCX_TLS=self,tcp
 # Run CP2K regression tests
 export CP2K_DATA_DIR="${PREFIX}/share/cp2k/data"
 export OMP_STACKSIZE=256M
+${PREFIX}/bin/cp2k -v
 "${PWD}/tests/do_regtest.py" "${PREFIX}/bin" "psmp" \
   --maxtasks "${CPU_COUNT}" \
   --smoketest \
   --workbasedir "${BUILD_PREFIX}"
-# For a full regression test without --smoketest add
-#  --skipdir "QS/regtest-dcdft-hfx" \
-#  --skipdir "SIRIUS/regtest-1" \
-# to make it pass without error
